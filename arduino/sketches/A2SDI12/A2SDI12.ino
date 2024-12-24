09097e4f3dca544036ddae3a9d0421f36ce8f8f8
thkrz <thomas.kreuzer@rwth.aachen.de>
2024-12-24 22:55:09 +0100

    Cast a Giant Shadow

--- arduino/sketches/A2SDI12/A2SDI12.ino
+++ arduino/sketches/A2SDI12/A2SDI12.ino
@@ -3,19 +3,38 @@
 
 #define CMD_LEN 32
 #define EE_ADDR 0
-#define NUM_SEN 6
+#define NUM_CON 6
 
+struct Connector {
+  char addr;
+  bool active;
+  float odd, even;
+};
+
+Connector conn[NUM_CON];
 SDI12 socket(1);
-Sensor sens[NUM_SEN];
-char tab[NUM_SEN];
 char buf[CMD_LEN]
 int len = 0;
+int pin[] = {
+  A0, A1, A2, A3, A4, A5,
+  A6, A7, A8, A9, A10, A11
+};
 
 int index(char a) {
   for (int i = 0; i < NUM_SEN; i++)
-    if (tab[i] == a)
+    if (conn[i].active && conn[i].addr == a)
       return i;
   return -1;
+}
+
+void measure(int i) {
+  int k = i * 2;
+  for (int j = 0; j < 3; j++) {
+    conn[i].even += analogRead(pin[k]);
+    conn[i].odd += analogRead(pin[k+1]);
+  }
+  conn[i].even /= 3.0;
+  conn[i].odd /= 3.0;
 }
 
 void rc() {
@@ -28,18 +47,18 @@ void rc() {
   if (len > 1)
     switch (buf[1]) {
     case 'I':
-      r = sens[i].ident();
+      r = "";
       break;
     case 'M':
       m = true;
-      r = sens[i].prepare();
+      r = "0012";
       break;
     case 'D':
-      r = sens[i].data(buf[2]);
+      r = data(i);
       break;
     case 'A':
       addr = buf[2];
-      tab[i] = addr;
+      conn[i].addr = addr;
       EEPROM.write(EE_ADDR+i, addr);
       break;
     }
@@ -47,12 +66,18 @@ void rc() {
   String s = String(addr) + r + "\r\n";
   socket.sendResponse(s);
   if (m)
-    sens[i].measure();
+    measure(i);
 }
 
 void setup() {
-  for (int i = 0; i < NUM_SEN; i++)
-    tab[i] = EEPROM.read(EE_ADDR+i);
+  for (int i = 0; i < NUM_CON; i++) {
+    int j = i * 2;
+    pinMode(pin[j], INPUT_PULLUP);
+    conn[i].active = analogRead(pin[j]) > 0;
+    pinMode(pin[j], INPUT);
+    if (active)
+      conn[i].addr = EEPROM.read(EE_ADDR+i);
+  }
   socket.begin();
   delay(100);
   socket.forceListen();
