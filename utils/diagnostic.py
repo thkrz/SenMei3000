c443a0074371dcaf6695e2ae12f7e437e7bd1d9f
thkrz <thomas.kreuzer@rwth.aachen.de>
2025-01-26 08:26:21 +0100

    Mercenary

---
+++ utils/diagnostic.py
@@ -0,0 +1,26 @@
+import serial
+import time
+
+port = "/dev/ttyACM0"
+com = serial.Serial(port)
+
+
+def loop():
+    s = com.readline()
+    print(s)
+
+
+if __name__ == "__main__":
+    com.baudrate = 9600
+    com.bytesize = 8
+    com.parity = "N"
+    com.stopbits = 1
+    time.sleep(3)
+
+    try:
+        while True:
+            loop()
+    except EOFError:
+        pass
+    except KeyboardInterrupt:
+        pass
