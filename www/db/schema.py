4e541db66a673a49a67173257839db473e8d8126
thkrz <thomas.kreuzer@rwth.aachen.de>
2025-01-27 23:46:53 +0100

    A Star to Guide Them

---
+++ www/db/schema.py
@@ -0,0 +1,20 @@
+o = {
+    "sid": "1",
+    "x": 49.234,
+    "y": 37.454,
+    "sensor_schema": {
+        "a": [
+            {"Measurand": "Volumetric Water Content", "Unit": "-"},
+            {"Measurand": "Temperature", "Unit": "°C"},
+        ]
+    },
+    "timeseries": [
+        {
+            "date": None,
+            "bat": 12.1,
+            "temp": 23.1,
+            "rh": 59.5,
+            "data": {"a": [12.2, 14.1]},
+        }
+    ],
+}
