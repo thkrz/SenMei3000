3811a6cd53ba81a54a04c66a32d30df9bc43427b
thkrz <thomas.kreuzer@rwth.aachen.de>
2025-01-28 22:54:56 +0100

    Surprise

--- utils/diagnostic.py
+++ utils/diagnostic.py
@@ -2,27 +2,18 @@ import serial
 import time
 
 port = "/dev/ttyACM0"
-com = serial.Serial(port)
-
-
-def loop():
-    cmd = input("RYUJIN> ")
-    com.write(cmd.encode())
-    s = com.readline()
-    print(s)
 
 
 if __name__ == "__main__":
+    com = serial.Serial(port)
     com.baudrate = 9600
     com.bytesize = 8
     com.parity = "N"
     com.stopbits = 1
     time.sleep(3)
-
-    try:
-        while True:
-            loop()
-    except EOFError:
-        pass
-    except KeyboardInterrupt:
-        pass
+    com.write("!".encode())
+    ln = ""
+    while ln != "!":
+        ln = com.readline()
+        print(ln, end="")
+    com.close()
