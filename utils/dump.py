cd34159c866e6274efb67f49f49887fbe4096e23
Thomas Kreuzer <thomas.kreuzer@uni-wuerzburg.de>
2025-01-29 10:55:44 +0100

    The Apple

---
+++ utils/dump.py
@@ -0,0 +1,19 @@
+import serial
+import time
+import sys
+
+port = "/dev/ttyACM0"
+
+
+if __name__ == "__main__":
+    com = serial.Serial(port)
+    com.baudrate = 9600
+    com.bytesize = 8
+    com.parity = "N"
+    com.stopbits = 1
+    time.sleep(1)
+    com.write("!".encode())
+    s = com.read_until("#".encode())[:-1]
+    com.close()
+    sys.stdout.write(s.decode("utf-8"))
+    sys.stdout.flush()
