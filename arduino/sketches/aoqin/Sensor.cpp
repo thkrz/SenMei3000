a1aabc28183bf9e765477e11be0739bb2a288a8e
thkrz <thomas.kreuzer@rwth.aachen.de>
2025-03-19 21:58:06 +0100

    Gabrielle's Hope

--- arduino/sketches/aoqin/Sensor.cpp
+++ arduino/sketches/aoqin/Sensor.cpp
@@ -3,22 +3,21 @@
 #define A (100.0/3.0)
 #define B 1.2
 
-static void smt100data(float*, float**, int*);
-
+/* Truebner SMT100
+ * https://www.truebner.de/de/smt100.php
+ */
 Sensor SMT100 = {
-  smt100data,
+  [](float *u, float **w, int *len) {
+    static float v[5];
+
+    *len = 5;
+    v[0] = u[0];
+    v[1] = 0;
+    v[2] = u[0] * A;
+    v[3] = (u[1] - B) * A;
+    v[4] = 0;
+    *w = v;
+  },
   String("13TRUEBNERSMT100038241127"),
   String("0022")
 };
-
-void smt100data(float *u, float **w, int *len) {
-  static float v[5];
-
-  *len = 5;
-  v[0] = u[0];
-  v[1] = 0;
-  v[2] = u[0] * A;
-  v[3] = (u[1] - B) * A;
-  v[4] = 0;
-  *w = v;
-}
