59864a06be8539c12d99bade9c9a5e5eedfa1db8
Thomas Kreuzer <thomas.kreuzer@uni-wuerzburg.de>
2025-05-09 13:17:39 +0200

    Orphan of War

--- www/trd.sh
+++ www/trd.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-n=$1
-for data in post/*.txt; do
-  curl -X POST -H "Content-Type: text/plain" --data-binary "@${data}" -i "http://erdrutsch.com:8000/station/TEST1"
-  n=$((n-1))
-  if [ $n -eq 0 ]; then
-    break
-  fi
-done
+curl -X POST -H "Content-Type: text/plain" --data-binary "@data.txt" -i "http://erdrutsch.com:8000/station/$1"
