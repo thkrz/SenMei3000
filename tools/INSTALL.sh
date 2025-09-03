0eb5881dd7cc7ea07a190732e4c265811c3c8efb
thkrz <thomas.kreuzer@rwth.aachen.de>
2025-09-03 17:15:34 +0200

    Antony and Cleopatra

--- tools/INSTALL.sh
+++ tools/INSTALL.sh
@@ -2,5 +2,8 @@
 PREFIX="$HOME/.local"
 python3 -m zipapp ryujin -o ryujin.pyz -p "/usr/bin/python3"
 install -m755 ryujin.pyz "$PREFIX/bin"
-desktop-file-install Ryujin.desktop
+install -m644 Ryujin.desktop "$PREFIX/share/applications"
+if [ -d "$(xdg-user-dir DESKTOP)" ]; then
+  install -m644 Ryujin.desktop "$(xdg-usr-dir DESKTOP)"
+fi
 rm -f ryujin.pyz
