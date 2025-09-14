30ada874e379738386b8bab0b6e4d3bec81570d0
thkrz <thomas.kreuzer@rwth.aachen.de>
2025-09-14 23:16:22 +0200

    Brothers and Sisters

---
+++ arduino/sketches/ryujin/gps.cpp
@@ -0,0 +1,31 @@
+#include "gps.h"
+#include <TinyGPSPlus.h>
+
+bool GPS::_parse() {
+  return true;
+}
+
+bool GPS::begin() { Serial.begin(4800); }
+
+void GPS::close() { Serial.close(); }
+
+bool GPS::wait() {
+  bool sentence = false;
+  uint32_t t0 = millis();
+  while (millis() - t0 < GPS_TIMEOUT)
+    if (Serial.available()) {
+      char c = Serial.read();
+      if (c == '$') {
+        _len = 0;
+        sentence = true;
+        continue;
+      }
+      if (sentence) {
+        if (_len < MAX_NMEA)
+          _buf[_len++] = c;
+        if (c == '\n')
+          return _parse();
+      }
+    }
+  return false;
+}
