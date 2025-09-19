950b88dcd59865eaeafa93ce82cd937cb7e26e81
thkrz <thomas.kreuzer@rwth.aachen.de>
2025-09-19 16:57:20 +0200

    The Festival of Dionysus

---
+++ tools/ryujin-gsm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+FBQN=arduino:samd:mkrnb1500
+
+SKETCH=../arduino/tests/tgsm
+arduino-cli compile -b ${FBQN} $SKETCH
+PORT=$(arduino-cli board list | awk "/${FBQN}/"'{print $1}')
+if [ -n "$PORT" ]; then
+	arduino-cli upload -p ${PORT} -b ${FBQN} $SKETCH
+	sleep 2
+  picocom -b 9600 $PORT
+else
+	echo "no board found" >&2
+fi
