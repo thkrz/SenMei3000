3f6d235d3b320c871c09111b22b7fb752735ca03
Thomas Kreuzer <thomas.kreuzer@uni-wuerzburg.de>
2025-09-19 12:58:45 +0200

    As Darkness Falls

--- tools/ryujin-init.sh
+++ tools/ryujin-init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-FIRMWARE="\"$(cat fw.txt)\""
 FBQN=arduino:samd:mkrnb1500
 
 if [ -n "$1" ]; then
@@ -12,6 +11,7 @@ else
 fi
 
 SKETCH=../arduino/sketches/ryujin
+FIRMWARE="\"$(cat $SKETCH/fw.txt)\""
 DEF="-DFIRMWARE=$FIRMWARE -DAPN=\"iot.1nce.net\" -DMI_MINUTE=15 -DLEGACY_BUILT=1"
 arduino-cli compile -b ${FBQN} --build-property="compiler.cpp.extra_flags=$DEF" $SKETCH
 PORT=$(arduino-cli board list | awk "/${FBQN}/"'{print $1}')
